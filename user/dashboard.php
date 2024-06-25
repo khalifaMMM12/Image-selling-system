51bdfd207d3ae91f82254f17505836a7eb4d7f73
khalifammm12 <khalifasitemsn@gmail.com>
2024-06-25 18:28:44 +0100

    user dashbord updated

--- user/dashboard.php
+++ user/dashboard.php
@@ -27,6 +27,7 @@ $result = $conn->query($sql);
             <li><a href="dashboard.php">Dashboard</a></li>
             <li><a href="cart.php">Cart</a></li>
             <li><a href="orders.php">My Orders</a></li>
+            <li><a href="upload_image.php">Upload Image</a></li>
             <li><a href="../logout.php">Logout</a></li>
         </ul>
     </nav>
@@ -39,7 +40,7 @@ $result = $conn->query($sql);
                 echo "<img src='../images/{$row['filename']}' alt='{$row['title']}'><br>";
                 echo "<h3>{$row['title']}</h3>";
                 echo "<p>{$row['description']}</p>";
-                echo "<p>Price: \${$row['price']}</p>";
+                echo "<p>Price: \₦{$row['price']}</p>";
                 echo "<form method='post' action='add_to_cart.php'>";
                 echo "<input type='hidden' name='image_id' value='{$row['id']}'>";
                 echo "<button type='submit'>Add to Cart</button>";
